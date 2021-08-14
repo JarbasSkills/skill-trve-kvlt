4fc03798e30923408fa9c428b170afc2b692872f
jarbasal <jarbasai@mailfence.com>
2021-08-14 14:02:15 +0100

    .

--- __init__.py
+++ __init__.py
@@ -3,9 +3,10 @@ from os.path import join, dirname, basename
 from mycroft.skills.core import intent_file_handler
 from pyvod import Collection, Media
 from os.path import join, dirname, basename
-from ovos_workshop.frameworks.playback import CPSMatchType, CPSPlayback, \
-    CPSMatchConfidence
+from ovos_workshop.frameworks.playback import CommonPlayMediaType, CommonPlayPlaybackType, \
+    CommonPlayMatchConfidence
 from ovos_workshop.skills.video_collection import VideoCollectionSkill
+import biblioteca
 
 
 class BlackMetalSkill(VideoCollectionSkill):
@@ -17,11 +18,11 @@ class BlackMetalSkill(VideoCollectionSkill):
         self.skill_icon = join(dirname(__file__), "ui", "trvekvlt_icon.png")
         self.default_bg = join(dirname(__file__), "ui", "bg.png")
         self.message_namespace = basename(dirname(__file__)) + ".jarbasskills"
-        self.supported_media = [CPSMatchType.GENERIC,
-                                CPSMatchType.VIDEO,
-                                CPSMatchType.MUSIC]
-
-        path = join(dirname(__file__), "res", "trveKvlt.jsondb")
+        self.supported_media = [CommonPlayMediaType.GENERIC,
+                                CommonPlayMediaType.VIDEO,
+                                CommonPlayMediaType.MUSIC]
+        base_folder = biblioteca.download("ytcat_trveKvlt")
+        path = join(base_folder, "trveKvlt.jsondb")
         # load video catalog
         self.media_collection = Collection("TrveKvlt",
                                            logo=self.skill_logo, db_path=path)
@@ -38,11 +39,11 @@ class BlackMetalSkill(VideoCollectionSkill):
         score = 0
 
         if self.voc_match(phrase,
-                          "video") or media_type == CPSMatchType.VIDEO:
+                          "video") or media_type == CommonPlayMediaType.VIDEO:
             score += 1
 
         if self.voc_match(phrase,
-                          "music") or media_type == CPSMatchType.MUSIC:
+                          "music") or media_type == CommonPlayMediaType.MUSIC:
             score += 10
 
         if self.voc_match(phrase, "trve"):
