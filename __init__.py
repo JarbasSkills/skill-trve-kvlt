8bdc9691738808fb8b112911ab48c9e6bea68a76
JarbasAi <jarbasai@mailfence.com>
2024-01-06 05:26:48 +0000

    refactor/ocp

--- __init__.py
+++ __init__.py
@@ -1,117 +1,62 @@
 import random
 from os.path import join, dirname
 
-from ovos_plugin_common_play.ocp import MediaType, PlaybackType
-from ovos_utils.log import LOG
-from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill, \
-    ocp_search, ocp_featured_media
-from youtube_archivist import YoutubeMonitor
+import requests
+from json_database import JsonStorageXDG
+
+from ovos_utils.ocp import MediaType, PlaybackType
+from ovos_workshop.decorators.ocp import ocp_search, ocp_featured_media
+from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill
 
 
 class BlackMetalSkill(OVOSCommonPlaybackSkill):
 
-    def __init__(self):
-        super().__init__("TRVEKVLT")
+    def __init__(self, *args, **kwargs):
         self.skill_icon = join(dirname(__file__), "ui", "trvekvlt_icon.png")
         self.default_bg = join(dirname(__file__), "ui", "bg.png")
-        self.supported_media = [MediaType.GENERIC,
-                                MediaType.AUDIO,
+        self.supported_media = [MediaType.AUDIO,
                                 MediaType.MUSIC]
-        self.archive = YoutubeMonitor(db_name="TrveKvlt", logger=LOG)
+        self.archive = JsonStorageXDG("TrveKvlt", subfolder="OCP")
+        super().__init__(*args, **kwargs)
 
     def initialize(self):
+        self._sync_db()
+        self.load_ocp_keywords()
+
+    def load_ocp_keywords(self):
+        albums = []
+        artists = []
+        songs = []
+        genre = ["Black Metal"]
+
+        for url, data in self.archive.items():
+            t = data["title"].split("(")[0].split("|")[0].split("[")[0].strip() \
+                .replace("‎-", "-")
+            if "-" in t:
+                artist, title = t.split("-")[:2]
+                artists.append(artist.strip())
+                if "album" in data["title"].lower():
+                    albums.append(title.strip())
+                else:
+                    songs.append(title.strip())
+
+        self.register_ocp_keyword(MediaType.MUSIC,
+                                  "artist_name", artists)
+        self.register_ocp_keyword(MediaType.MUSIC,
+                                  "album_name", albums)
+        self.register_ocp_keyword(MediaType.MUSIC,
+                                  "music_genre", genre)
+        self.register_ocp_keyword(MediaType.MUSIC,
+                                  "song_name", songs)
+
+    def _sync_db(self, message=None):
         bootstrap = f"https://github.com/JarbasSkills/skill-trve-kvlt/raw/dev/bootstrap.json"
-        self.archive.bootstrap_from_url(bootstrap)
+        data = requests.get(bootstrap).json()
+        self.archive.merge(data)
         self.schedule_event(self._sync_db, random.randint(3600, 24 * 3600))
 
-    def _sync_db(self):
-        urls = [
-            "https://www.youtube.com/watch?v=kU0pOmzj70o",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfWTRkRauhAjPh0h3ddMJin0",
-            "https://www.youtube.com/playlist?list=PLoul9rkF5MasLz-yl4ZLXymHkYrpFz7Bs",
-            "https://www.youtube.com/playlist?list=PLnFvfzGgiy7_3Hk2ikc6vd4HvzSdp7ft6",
-            "https://www.youtube.com/user/NihiLizTikVoiD",
-            "https://www.youtube.com/user/Adnan3098",
-            "https://www.youtube.com/c/Werwolf/videos",
-            "https://www.youtube.com/watch?v=7njdEm4-7MU",
-            "https://www.youtube.com/watch?v=uNtO0_CYeaI",
-            "https://www.youtube.com/playlist?list=PLbkph_GtwePZmD-bruvFbuaDnmIJMBjyZ",
-            "https://www.youtube.com/playlist?list=PLm7wnjUQm_FC0AClRhesJXfviQ0A4IvPq",
-            "https://www.youtube.com/channel/UCVEf05n0AptM-N1RzeYJn-g/videos",
-            "https://www.youtube.com/channel/UCJs_uIwVoNCKZAQ5jDqTlkw/videos",
-            "https://www.youtube.com/c/NoSolace/videos",
-            "https://www.youtube.com/watch?v=UGOwy3fgnUE",
-            "https://www.youtube.com/c/AtmosphericBlackMetalAlbumsOfficial/videos",
-            "https://www.youtube.com/c/666unholymartyr666/videos",
-            "https://www.youtube.com/playlist?list=OLAK5uy_kQ_YuC4aoOYiblVOXDKOzglBELweOtfyM",
-            "https://www.youtube.com/playlist?list=OLAK5uy_kUM73tmSd1Ds20-R1DDHUXG1UXaGYRmBw",
-            "https://www.youtube.com/playlist?list=PLJmjhSfzVjAMadhqNAs_PznWhKDyxnxtd",
-            "https://www.youtube.com/playlist?list=PLJmjhSfzVjAPijd95sOH3x9bzMIxloz8j",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfW6kmo4Io16o9eCpsR1fe7R",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUOPubujWEeh1fMJnL9DWSi",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUZpRNS4O72txUzPW6NQCOO",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUkXyyncMaTokT27WdkyMYg",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfXXqr1QWJ_h3l4RTrPlgxAv",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfXjYUvYqzvQLVPFMSFTUabS",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfVbG03H0pw72T623z4rSXmb",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfXEkgCh0X-KY62chUVAgRSH",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfWHT_OQmnnkj3FnwTR5xYtk",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfW8utcSHKtDudFjqTSX82yf",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUONxfFCEGKCi4Uq676skR7",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfV18spo8YlL0Ayu9MqVrxKx",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUHYqFB5HnY5uhYSKPSYBnQ",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfXxVouOUh_GZjLJP9_Esgig",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfWLSZlBqixP54DoTrrPPn_F",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfVKfruimJtofLGX-tUGlhWT",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfXywfjIDudxv5Dwe_R1kHCI",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUAUmsV4qcyDbBuvE2qO57V",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfVjl_ar1Jto9lQlr5zRiq21",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUJBLyMV6upmrMOVNDtBuWt",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUcCDcAKE_SqPMg5vfRjD1h",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfVU2JYRqzT4fttLCqmn9h1u",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfWTLQqFMFHLNdNMWXEzfCRt",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfWmlKgInvh5t6QzmAMmkJQu",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfWfPJCBPs6Jjj8NqgoOI-4o",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUB1RJOUUq_Za4kKeToEmsA",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfVzMTtxPb1ny4L4cBdNgF2J",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfXvQGPDKyqnyuKYJu3XHYVT",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfW6E5-g9kJy1y7LKO6veU4w",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfVJOEE550anPrXbl0mdRcS3",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfX1wBNu-xzWrXnIIcta2kMq",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfVypjq7JW-Ep3iceVoaHfW4",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfWFUqmxptBtKc9gT9bhRzRz",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUwGEasW2T_DLGPFj6E-EgZ",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfVwSNl_ZYDK4Iq3MPKf2jNE",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfWzxSRA1kRmIOnqCr6k69bG",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfVfdKf7bgQ-4kAQ2_N2JBSB",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUcaEY9lsaZ3meoVzY1M97O",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUkkzHxv_aSHAHCqLaGz_Mn",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfVrinRqF5sLNXOEHUIqqwf0",
-            "https://www.youtube.com/playlist?list=PLykNNMVjCDfUKFWDRi6mc4M7FA4eCjGaT",
-            "https://www.youtube.com/watch?v=rSHf81j_PQE",
-            "https://www.youtube.com/watch?v=ChGALXClzH4",
-            "https://www.youtube.com/watch?v=MiCLnDDZ4w0",
-            "https://www.youtube.com/watch?v=BJAnJ8b6q5M",
-            "https://www.youtube.com/watch?v=D50XrYiinNc",
-            "https://www.youtube.com/c/bmpromotion/videos",
-        ]
-        for url in urls:
-            self.archive.parse_videos(url)
-        self.schedule_event(self._sync_db, random.randint(3600, 24*3600))
-
-    # matching
-    def match_skill(self, phrase, media_type):
-        score = 0
-        if self.voc_match(phrase, "music") or media_type == MediaType.MUSIC:
-            score += 10
-        if self.voc_match(phrase, "trve"):
-            score += 10
-        if self.voc_match(phrase, "black-metal"):
-            score += 90
-        return score
-
     def get_playlist(self, score=50, num_entries=100, idx=1):
-        entries = list(self.archive.db.items())
+        entries = list(self.archive.items())
         random.shuffle(entries)
         entries = entries[:num_entries]
         pl = [{
@@ -138,13 +83,50 @@ class BlackMetalSkill(OVOSCommonPlaybackSkill):
 
     @ocp_search()
     def search_db(self, phrase, media_type):
-        base_score = self.match_skill(phrase, media_type)
-        if base_score >= 50:
+        base_score = 15 if media_type == MediaType.MUSIC else 0
+        entities = self.ocp_voc_match(phrase)
+
+        base_score += 30 * len(entities)
+
+        candidates = list(self.archive.values())
+        artist = entities.get("artist_name")
+        album = entities.get("album_name")
+        song = entities.get("song_name")
+
+        if artist or song or album:
+            # filter valid results
+            if album:
+                candidates = [video for video in candidates
+                              if album.lower() in video["title"].lower()]
+            elif artist:
+                candidates = [video for video in candidates
+                              if artist.lower() in video["title"].lower()]
+            elif song:
+                candidates = [video for video in candidates
+                              if song.lower() in video["title"].lower()]
+
+            for video in candidates:
+                yield {
+                    "title": video["title"],
+                    "author": artist or video["author"],
+                    "album": album,
+                    "match_confidence": min(100, base_score),
+                    "media_type": MediaType.AUDIOBOOK,
+                    "uri": "youtube//" + video["url"],
+                    "playback": PlaybackType.AUDIO,
+                    "skill_icon": self.skill_icon,
+                    "skill_id": self.skill_id,
+                    "image": video["thumbnail"],
+                    "bg_image": self.default_bg
+                }
+
+        # black metal playlists on black metal request
+        if "music_genre" in entities:
             for i in range(5):
                 yield self.get_playlist(base_score, idx=i + 1)
 
     @ocp_featured_media()
-    def featured_media(self, num_entries=250):
+    def featured_media(self, num_entries=50):
         return [
                    {
                        "match_confidence": 100,
@@ -156,9 +138,16 @@ class BlackMetalSkill(OVOSCommonPlaybackSkill):
                        "bg_image": self.default_bg,
                        "skill_icon": self.skill_icon,
                        "title": entry["title"]
-                   } for entry in self.archive.sorted_entries()
+                   } for entry in self.archive.values()
                ][:num_entries]
 
 
-def create_skill():
-    return BlackMetalSkill()
+if __name__ == "__main__":
+    from ovos_utils.messagebus import FakeBus
+
+    s = BlackMetalSkill(bus=FakeBus(), skill_id="t.fake")
+    for r in s.search_db("burzum", MediaType.MUSIC):
+        print(r)
+
+    for r in s.search_db("xasthur", MediaType.MUSIC):
+        print(r)
